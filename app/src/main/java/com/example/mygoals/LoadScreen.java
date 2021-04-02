c067520ad71908ebe037d10137579be71182e641
manoharrk22 <manoharrk36@gmail.com>
2021-04-02 20:58:02 +0530

    Fix logout btn

--- app/src/main/java/com/example/mygoals/LoadScreen.java
+++ app/src/main/java/com/example/mygoals/LoadScreen.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class LoadScreen extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+    public class LoadScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
+
+
 
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
--- app/src/main/java/com/example/mygoals/TabActivity.java
+++ app/src/main/java/com/example/mygoals/TabActivity.java
@@ -1,5 +1,6 @@
 package com.example.mygoals;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -16,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.example.mygoals.ui.main.SectionsPagerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TabActivity extends AppCompatActivity {
 
@@ -29,12 +31,22 @@ public class TabActivity extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton logout=(FloatingActionButton)findViewById(R.id.buttonlogout) ;
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(TabActivity.this, Login.class);
+                startActivity(i);
             }
         });
     }
